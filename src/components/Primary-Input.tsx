92fd202cb946a9249d3d3cdf58ab1cee8f620bb0
Vinícius de Oliveira <112340022+vinefps@users.noreply.github.com>
2025-03-22 17:24:10 -0300

    Update Primary-Input.tsx

--- src/components/Primary-Input.tsx
+++ src/components/Primary-Input.tsx
@@ -2,20 +2,18 @@
 import { TypeHeader } from "@/app/types/product";
 import { Button } from "./Button";
 
-
 export function PrimaryInput({ setSearchTerm }: TypeHeader) {
-
   return (
-    <div>
+    <div className="relative flex w-full">
       <input
         onChange={(e) => {
           if (setSearchTerm) {
             setSearchTerm(e.target.value);
           }
         }}
-        className="bg-gray-100 outline-none p-2 rounded-md"
+        className="w-full bg-gray-100 border border-gray-200 outline-none py-2 px-4 rounded-l-md focus:ring-2 focus:ring-blue-300 focus:border-transparent transition-all duration-200 placeholder-gray-400"
         type="text"
-        placeholder="Nome do Produto"
+        placeholder="Buscar produtos..."
       />
       <Button />
     </div>
